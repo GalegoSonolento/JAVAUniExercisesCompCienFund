1e33c78e73cb44e1d5553d57c30b6f2d7ddeaedf
HenryH <henriquehaag11@gmail.com>
2022-05-11 12:05:26 -0300

    Commit

--- Aula_23/src/Teste.java
+++ Aula_23/src/Teste.java
@@ -1,5 +1,7 @@
+import java.nio.charset.CoderResult;
+
 public class Teste {
-    public static void main(String args[]){
+    public static void main(String [] args){
         Competicao comp = new Competicao("Correr cansa", new Data(19, 5, 2011));
         System.out.println(comp.toString());
 
@@ -12,5 +14,19 @@ public class Teste {
 
         Atleta c = new Corredor("Augosto", 100, 70, new Competicao("São Silvestre", new Data(1, 1, 2012)));
         ((Corredor)c).imprimeInfo();
+
+        Atleta d = null;
+        int opcao = Teclado.leInt("Desea criar um nadador [1] ou um corredor [2]? ");
+        if (opcao == 1) d = new Nadador(Teclado.leString("Digite o nome do nadador: "), Teclado.leInt("Digite a idade deste nadador: "), Teclado.leString("Digite a categoria deste nadador: "));
+        else if (opcao == 2) d = new Corredor(Teclado.leString("Digite o nome deste corredor: "), Teclado.leInt("Digite a idade deste corredor: "), Teclado.leDouble("Digite o peso deste corredor: "), new Competicao(Teclado.leString("Digite o nome da competição: "), new Data(Teclado.leInt("Digite o dia: "), Teclado.leInt("Digite o mês: "), Teclado.leInt("Digite o ano: "))));
+
+        InformacoesAtletas info1 = new InformacoesAtletas();
+        info1.imprimeExclusivosAtleta(d);
+        info1.imprimeInformacoesAtleta(d);
+
+        if (d instanceof Nadador) ((Nadador)d).setCategoria("Livre");
+        else if (d instanceof Corredor) ((Corredor)d).setPeso(89);
+
+        info1.imprimeExclusivosAtleta(d);
     }
 }
