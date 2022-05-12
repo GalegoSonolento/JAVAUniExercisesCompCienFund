6dd6d7ef25c81ea880b01b74a9cffc6958a4d5b1
HenryH <henriquehaag11@gmail.com>
2022-05-12 12:45:29 -0300

    Commit

---
+++ Aula_24/src/Teste.java
@@ -0,0 +1,19 @@
+public class Teste {
+    public static void main(String[] args){
+
+        // Exercício 1
+        //a)
+        int[] a;
+        a = new int[10];
+        int j = 0;
+        for (int i=10; i > 0; i--){
+            a[j] = i;
+            j ++;
+        }
+        j = 0;
+        for (int c=)
+        for (int k=0; k<10; k++) {
+            System.out.println(a[k]);
+        }
+    }
+}
