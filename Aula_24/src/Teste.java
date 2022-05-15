bdb6f247df89252114e47c247a43a49556a3e367
HenryH <henriquehaag11@gmail.com>
2022-05-15 15:06:17 -0300

    Commit

--- Aula_24/src/Teste.java
+++ Aula_24/src/Teste.java
@@ -1,41 +1,104 @@
 public class Teste {
     public static void main(String[] args){
 
-        // Exercício 1
-        //a)
-        int[] a;
-        a = new int[10];
-        int j = 0;
-        for (int i=10; i > 0; i--){
-            a[j] = i;
-            j ++;
-        }
-        //b)
-        j = 0;
-        int baixa = 0;
-        int alta = 0;
-        int[] b;
-        b = new int[11];
-        b[j] = 0;
-        j ++;
-        for (int c=0; c<101; c++){
-            if (c == 0){
-                baixa = 1;
-                alta = 4;
-            }
-            if (c == baixa){
-                b[j] = baixa;
-                j ++;
-                baixa *= alta;
-            }
-            else if (c == alta){
-                b[j] = alta;
-                j ++;
-                alta *= baixa;
-            }
-        }
-        for (int k=0; k<10; k++) {
-            System.out.println(a[k]);
-        }
+//        // Exercício 1
+//        //a)
+//        int[] a;
+//        a = new int[10];
+//        int j = 0;
+//        for (int i=10; i > 0; i--){
+//            a[j] = i;
+//            j ++;
+//        }
+//        //b)
+//        j = 0;
+//        int baixa = 0;
+//        int alta = 0;
+//        int[] b;
+//        b = new int[11];
+//        b[j] = 0;
+//        j ++;
+//        for (int c=0; c<101; c++){
+//            if (c == 0){
+//                baixa = 1;
+//                alta = 4;
+//            }
+//            if (c == baixa){
+//                b[j] = baixa;
+//                j ++;
+//                baixa *= alta;
+//            }
+//            else if (c == alta){
+//                b[j] = alta;
+//                j ++;
+//                alta *= baixa-1;
+//            }
+//        }
+//
+//        //c) 1 2 3 4 5 10 20 30 40 50
+//        int[] c;
+//        c = new int[10];
+//        int cont = 0;
+//        for (int l=1; l<51; l++){
+//            if (l <= 5){
+//                c[cont] = l;
+//                cont ++;
+//            }
+//            else{
+//                if (l == 10) {
+//                    c[cont] = l;
+//                    cont++;
+//                }
+//                else if (l == 20) {
+//                    c[cont] = l;
+//                    cont++;
+//                }
+//                else if (l == 30) {
+//                    c[cont] = l;
+//                    cont++;
+//                }
+//                else if (l == 40) {
+//                    c[cont] = l;
+//                    cont++;
+//                }
+//                else if (l == 50) {
+//                    c[cont] = l;
+//                    cont++;
+//                }
+//            }
+//        }
+//        for (int k=0; k<10; k++) {
+//            //System.out.println(a[k]);
+//            //System.out.println(b[k]);
+//            //System.out.println(c[k]);
+//        }
+
+
+//        //Exercício 2
+//        double[] a;
+//        a = new double[5];
+//        for (int i=0;i<5;i++) a[i] = i;
+//        for (int j=0;j<5;j++) System.out.print(a[j]+"-");
+
+//        //Exercício 3
+//        double[] d = new double[10];
+//        for (int i=0; i<10; i++) d[i] = Math.random();
+//        for (int j=0;j<10;j++) System.out.println(d[j]);
+
+//        //Exercício 4
+//        int[] d = new int[5];
+//        int p = -2;
+//        int cont = 0;
+//        for (int i=0; i<5; i++) {
+//            d[i] = p;
+//            p++;
+//        }
+//        for (int k=0; k<5; k++){
+//            if (d[k] < 0) cont ++;
+//        }
+//        System.out.println(cont);
+
+        //Exercício 5
+
     }
 }
