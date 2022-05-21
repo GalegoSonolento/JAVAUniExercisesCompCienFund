9e1600cf57b3e0d64722f89d8a58dc27c1f6616f
HenryH <henriquehaag11@gmail.com>
2022-05-21 15:27:26 -0300

    Commit

--- Aula_24/src/Baralho.java
+++ Aula_24/src/Baralho.java
@@ -1,4 +1,5 @@
 public class Baralho {
+    private Carta[] espadas, paus, copas, ouros;
     public Baralho() {
         Carta[] baralho = new Carta[56];
         for (int i=0; i<baralho.length; i++){
