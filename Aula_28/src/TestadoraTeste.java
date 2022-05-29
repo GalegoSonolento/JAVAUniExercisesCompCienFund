984c7bebb3cf057858caab86da9944b97f66d06b
HenryH <henriquehaag11@gmail.com>
2022-05-29 17:48:23 -0300

    Commit

---
+++ Aula_28/src/TestadoraTeste.java
@@ -0,0 +1,8 @@
+public class TestadoraTeste {
+    public static void main(String[] args){
+        MarcasEnum hp = MarcasEnum.HP;
+        MarcasEnum samsung = MarcasEnum.SAMSUNG;
+        System.out.println("Nome da marca: "+hp.name());
+        System.out.println("Nome da marca: "+samsung.name());
+    }
+}
