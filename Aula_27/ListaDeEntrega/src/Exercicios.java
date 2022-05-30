6d4561af68b1fbe12e6129529852661432dd4bd5
HenryH <henriquehaag11@gmail.com>
2022-05-30 10:26:05 -0300

    Commit

--- Aula_27/ListaDeEntrega/src/Exercicios.java
+++ Aula_27/ListaDeEntrega/src/Exercicios.java
@@ -58,18 +58,196 @@ public class Exercicios {
         return c;
     }
 
-//    public int[] exercicio7(int[] a, int[] b){
-//        int negs = 0;
-//        int posiv = 0;
-//        for (int i=0; i<a.length; i++)
-//            if (a[i] < 0) negs++;
-//            else if (a[i] >= 0) posiv++;
-//        negs = 0;
-//        posiv = 0;
-//        int[] tdNegativos = new int[negs];
-//        int[] tdPositivo = new int[posiv];
-//        for (int i=0; i<a.length; i++){
-//
-//        }
-//    }
+//    Exercício 7: Faça um método chamado exercicio7 que recebe dois arrays de inteiros, e retorna
+//    um terceiro array que possui todos os elementos negativos (dos dois arrays) na primeira parte
+//    e todos os elementos positivos (inclusive o zero) na segunda parte (dos dois arrays).
+
+    public int[] exercicio7(int[] a, int[] b){
+        int negs = 0;
+        int posiv = 0;
+        for (int i=0; i<a.length; i++)
+            if (a[i] < 0) negs++;
+            else if (a[i] >= 0) posiv++;
+        for (int i=0; i<b.length; i++)
+            if (b[i] < 0) negs++;
+            else if (b[i] >= 0) posiv++;
+        int[] tdNegativos = new int[negs];
+        int[] tdPositivo = new int[posiv];
+        negs = 0;
+        posiv = 0;
+        for (int i=0; i<a.length; i++)
+            if (a[i] < 0)
+                tdNegativos[negs++] = a[i];
+            else if (a[i] >= 0)
+                tdPositivo[posiv++] = a[i];
+        for (int i=0; i<b.length; i++)
+            if (b[i] < 0)
+                tdNegativos[negs++] = b[i];
+            else if (b[i] >= 0)
+                tdPositivo[posiv++] = b[i];
+        int[] resp = new int[tdPositivo.length+ tdNegativos.length];
+        int pos = 0;
+        for (int i=0; i< tdNegativos.length; i++)
+            resp[pos++] = tdNegativos[i];
+        for (int i=0; i< tdPositivo.length; i++)
+            resp[pos++] = tdPositivo[i];
+        return resp;
+    }
+
+//    Exercício 8: Faça um método chamado exercicio8 que recebe um array de char e imprime na
+//    tela (na mesma linha) todos os elementos do array
+    public void exercicio8(char[] c){
+        for (int i=0; i<c.length; i++)
+            System.out.print(c[i] + " ");
+    }
+
+//    Exercício 9: Faça um método chamado exercicio9 que recebe um array de inteiros e retorna a
+//    média de todos os elementos do array
+    public double exercicio9(int[] a){
+        double soma = 0;
+        for (int i=0; i<a.length; i++)
+            soma += a[i];
+        double tot = a.length;
+        return (soma/tot);
+    }
+
+//    Exercício 10: Faça um método chamado exercicio10 que recebe um array de inteiros e retorna
+//    a média de todos os elementos pares do array
+    public double exercicio10(int[] a){
+        double quantPares = 0;
+        double somaPares = 0;
+        for (int i=0; i<a.length; i++)
+            if (a[i] % 2 == 0){
+                quantPares += 1;
+                somaPares += a[i];
+            }
+        return somaPares/quantPares;
+    }
+
+//    Exercício 11: Faça um método chamado exercicio11 que recebe um array de inteiros e um
+//    inteiro x e retorna o elemento da posição x do array
+    public int exercicio11(int[] a, int x){
+        for (int i=0; i<a.length; i++)
+            if (i == x)
+                return a[i];
+        return -1;
+    }
+
+//    Exercício 12: Faça um método chamado exercicio12 que recebe um array de double e retorna
+//true caso algum elemento seja negativo, e false caso todos sejam positivos.
+    public boolean exercicio12(double[] a){
+        for (int i=0; i<a.length; i++)
+            if (a[i] < 0) return true;
+        return false;
+    }
+
+    public int exercicio13(int[] a){
+        int maior = 0;
+        for (int i=0; i<a.length; i++)
+            if (i == 0)
+                maior = a[i];
+            else
+                if (a[i] > maior)
+                    maior = a[i];
+        return maior;
+    }
+
+    public int exercicio14(int[] a){
+        int maiorNum = 0;
+        int maiorPos = -1;
+        for (int i=0; i<a.length; i++)
+            if (i == 0){
+                maiorPos = i;
+                maiorNum = a[i];
+            }
+            else
+                if (a[i] > maiorNum){
+                    maiorNum = a[i];
+                    maiorPos = i;
+                }
+        return maiorPos;
+    }
+
+    public int exercicio15(int[] a){
+        int menor = 0;
+        for (int i=0; i<a.length; i++)
+            if (i == 0)
+                menor = a[i];
+            else
+                if (a[i] < menor)
+                    menor = a[i];
+        return menor;
+    }
+
+    public int exercicio16(int[] a){
+        int menorNum = 0;
+        int menorPos = -1;
+        for (int i=0; i<a.length; i++)
+            if (i == 0){
+                menorNum = a[i];
+                menorPos = i;
+            }
+            else
+                if (a[i] < menorNum){
+                    menorNum = a[i];
+                    menorPos = i;
+                }
+        return menorPos;
+    }
+
+    public boolean exercicio17(int[] a, int[] b){
+        if (a.length != b.length) return false;
+        else
+            for (int i=0; i<a.length; i++)
+                if (a[i] != b[i]) return false;
+        return true;
+    }
+
+    public String exercicio18(char[] a){
+        String retorno = "";
+        for (int i=0; i<a.length; i++)
+            retorno += a[i];
+        return retorno;
+    }
+
+
+    public String[] exercicio19(int x){
+        String[] retorno = new String[x];
+        System.out.println("Digite os valores da array de Strings via teclado: ");
+        for (int i=0; i<retorno.length; i++)
+            retorno[i] = Teclado.leString("-> ");
+        return retorno;
+
+        //Essa aqui é só uma resposta que eu encontrei para o que eu pensei que fosse a resposta certa
+        // Ele vai criar um array de char que vai receber todos os algarismos de uma String
+        //Depois ele vai propriamente criar uma String e retornar o método
+//        char[] chars = new char[x];
+//        System.out.println("Digite os algarismos da String: ");
+//        for (int i=0; i<chars.length; i++)
+//            chars[i] = Teclado.leChar("");
+//        String retorno = "";
+//        for (int i=0; i<chars.length; i++)
+//            retorno += chars[i];
+//        return retorno;
+    }
+
+    public String[] exercicio20(String[] s, int x){
+        for (int i=0; i<s.length; i++)
+            if (i == x)
+                s[i] = null;
+        for (int i=0; i<s.length; i++)
+            if (i < s.length-1)
+                if (s[i] == null)
+                    if (s[i+1] != null){
+                        s[i] = s[i+1];
+                        s[i+1] = null;
+                    }
+        return s;
+    }
+
+    public void exercicio21(double[] d){
+        for (int i=0; i<d.length; i++)
+            if (d[i] >= 0)
+                System.out.println(i);
+    }
 }
--- Aula_27/ListaDeEntrega/src/Teste.java
+++ Aula_27/ListaDeEntrega/src/Teste.java
@@ -3,8 +3,19 @@ public class Teste {
         Exercicios ex = new Exercicios();
         int[] teste = {1,2,3,4,5,6,7,8,9,10};
         int[] teste2 = {23, 45, 1234, 7};
+        int[] teste3 = {-123, 23, -23, -9};
+        char[] teste4 = {'I', 'N', 'T', 'E', 'R'};
         String[] strings = {"Banana", "Melão", "Melancia", "Mandioca", "Tapicoa", "Maçã", "Pêra", "Açafrão", "Beterraba"};
         boolean[] booleans = {false, true, true, false};
+        double[] teste5 = {-4.123, 23, 4, 34.3};
+        double[] teste6 = {0.234, 34.2, 34.9};
+        int[] teste7 = {23, 45, 1234, 6};
+        char[] teste8 = {'G', 'R', 'E', 'M', 'I', 'S', 'T', 'A'};
+        String[] teste9 = {"Eu ", "não ", "gosto ", "de ", "laranja."};
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 1 =====");
+
         ex.exercicio1(teste);
         System.out.println(" ");
         System.out.println("=====Exercício 2 =====");
@@ -37,5 +48,79 @@ public class Teste {
         for (int i=0; i<retorno4.length; i++){
             System.out.println(retorno4[i]);
         }
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 7 =====");
+        int[] retorno5 = ex.exercicio7(teste, teste3);
+        for (int i=0; i<retorno5.length; i++){
+            System.out.println(retorno5[i]);
+        }
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 8 =====");
+        ex.exercicio8(teste4);
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("=====Exercício 9 =====");
+        double resp6 = ex.exercicio9(teste);
+        System.out.println(resp6);
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 10 =====");
+        System.out.println(ex.exercicio10(teste));
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("=====Exercício 11 =====");
+        System.out.println(ex.exercicio11(teste, 5));
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 12 =====");
+        System.out.println(ex.exercicio12(teste5));
+        System.out.println(ex.exercicio12(teste6));
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 13 =====");
+        System.out.println("O maior elemento do array de teste é: "+ex.exercicio13(teste2));
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 14 =====");
+        System.out.println("A posição do maior número inteiro da array de teste é: "+ex.exercicio14(teste2));
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 15 =====");
+        System.out.println("O menor elemento da array de teste é: "+ex.exercicio15(teste3));
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 16 =====");
+        System.out.println("A menor posição do menor número inteiro da array de teste é: "+ex.exercicio16(teste2));
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 17 =====");
+        System.out.println(ex.exercicio17(teste7, teste2) ? "Os arrays são iguais" : "Os arrays são diferentes");
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 18 =====");
+        System.out.println(ex.exercicio18(teste8));
+
+        System.out.println(" ");
+        System.out.println("=====Exercício 19 =====");
+        String[] resp7 = ex.exercicio19(Teclado.leInt("Digite quantas Strings quer no array de String: "));
+        for (int i=0; i<resp7.length; i++)
+            System.out.print(resp7[i]+" ");
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("=====Exercício 20 =====");
+        String[] resp8 = ex.exercicio20(teste9, 1);
+        for (int i=0; i<resp8.length; i++)
+            if (resp8[i] != null)
+                System.out.print(resp8[i]);
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("=====Exercício 21 =====");
+        ex.exercicio21(teste5);
     }
 }
