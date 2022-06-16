3a8e2f97bdb0a62ca4c461453ecfa0750f537452
Henrique Haag <96831480+GalegoSonolento@users.noreply.github.com>
2022-06-15 22:00:42 -0300

    Update Turma.java

--- TreinoGB/src/Turma.java
+++ TreinoGB/src/Turma.java
@@ -1,18 +1,3 @@
-/*(1,0	pt)	Crie	uma	classe	chamada	Turma.
-Esta	classe	possui	como	atributos	um	código	(tipo	String)	e	um	array
-de	Aluno.
-No	construtor,	receba	o	código	da	turma	e	a	quantidade	máxima de	alunos	por	parâmetro
-(Inicialize
-o	array	de	Aluno	com	esta	quantidade).
-Ainda	na	classe	Turma,	faça	o	que	se	pede:
-• (1,0	pt) crie	um	método	insereAluno,
-que	recebe	um	objeto	do	tipo	Aluno por	parâmetro	e	o	insere
-na	primeira	posição	livre	do	array,
-retornando	verdadeiro	caso	seja	possível	inserir	e	falso	caso	contrário
-• (1,0 pt) crie	 um	método	 chamado	mediaNotasTurma,	 que	 retorna
-a	média	 de	 nota	 (nota	 final)	 de
-todos	os	alunos	presentes	no	array	(ou	-1	caso	não	existam	alunos)*/
-
 public class Turma {
     private String codigo;
     private Aluno[] alunos;
